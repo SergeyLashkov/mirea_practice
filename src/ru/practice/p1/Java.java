b2d72b40dec4d744c9cc54a52a6c346a613aa268
Sergey_Lashkov <sergey230102@icloud.com>
2020-09-09 09:36:24 +0300

    Initial commit

--- src/ru/practice/p1/Java.java
+++ src/ru/practice/p1/Java.java
@@ -1,4 +1,12 @@
 package ru.practice.p1;
 
 public class Java {
+    public static void main(String[] args) {
+        Dog d1 = new Dog ("Mike", 12);
+        Dog d2 = new Dog ("Sharik");
+        Dog d3 = new Dog();
+        d1.intoHumanAge();
+        d2.setAge(4);
+        d2.intoHumanAge();
+    }
 }
